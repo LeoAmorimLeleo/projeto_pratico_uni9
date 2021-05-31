398676e4d610139c1bae8ad3ad256f4fbe8b49bc
Leonardo Amorim Ramos <leonardo.ramos-ext@ab-inbev.com>
2021-05-31 13:51:17 -0300

    ajustes no carrinho

--- pizzaria/WebContent/js/mycart.js
+++ pizzaria/WebContent/js/mycart.js
@@ -175,7 +175,7 @@
         '</div>' +
         '<div class="modal-footer">' +
         '<button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>' +
-        '<a href="#" class="btn btn-primary" id="' + idCheckoutCart + '">Confirmar</a>' +
+        '<a class="btn btn-primary" id="' + idCheckoutCart + '">Confirmar</a>' +
         '</div>' +
         '</div>' +
         '</div>' +
